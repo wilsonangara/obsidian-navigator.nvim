8eebcd12a0856c98b32a3825d29d91a36de61012
Wilson Angara <wilsonangara@gmail.com>
2025-06-01 16:35:42 +0700

    feat: add logic to validate whether opened file is inside obsidian vault
    This commit includes:
    - Adding config option for users to set list of absolute paths to
      obsidian vaults
    - Add logic to check whether the current working directory of a file is
      inside an obsidian vault which will determine to enable or disable plugin

--- lua/obsidian-navigator/config.lua
+++ lua/obsidian-navigator/config.lua
@@ -3,11 +3,14 @@ local M = {}
 -- Indicates whether the plugin is enabled or disabled.
 M.on = true
 
-M.setup_config = function()
+M.setup_config = function(user_config)
+	-- Defaults to an empty table if no user configuration is provided.
+	user_config = user_config or {}
+
 	-- Note: for now we are hardcoding the obsidian base URL.
 	local default_config = {
 		obsidial_base_url = "http://localhost:27124",
-		obsidian_vaults = {},
+		obsidian_vaults = user_config.vaults or {},
 	}
 	M.config = default_config
 end
--- lua/obsidian-navigator/init.lua
+++ lua/obsidian-navigator/init.lua
@@ -6,15 +6,26 @@ vim.api.nvim_create_augroup("obsidian-navigator.nvim", { clear = true })
 
 -- Initialize the plugin configuration.
 local function init()
-	-- Checks whether the plugin is enabled.
-	if not config.on then
+	-- Checks whether the current working directory, if yes turn on the plugin.
+	local cwd = vim.fn.getcwd()
+	local inside_directory = false
+	for _, path in ipairs(config.config.obsidian_vaults) do
+		-- Compare path to directory
+		if path == cwd:sub(1, #path + 1) then
+			config.on = true
+			inside_directory = true
+			break
+		end
+	end
+	if not inside_directory then
+		config.on = false
 		return
 	end
-
-	-- Checks whether the current working directory
 end
 
-function M.setup()
+function M.setup(user_config)
+	config.setup_config(user_config)
+
 	vim.api.nvim_create_autocmd("BufEnter", {
 		callback = init,
 		pattern = "*",
