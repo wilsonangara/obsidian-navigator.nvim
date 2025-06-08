34edbf56b116aa68521345d3890ec2b4868a1340
Wilson Angara <wilsonangara@gmail.com>
2025-06-08 22:47:57 +0700

    chore: put all methods under helper function to check if plugin is enabled

--- lua/obsidian-navigator/handlers.lua
+++ lua/obsidian-navigator/handlers.lua
@@ -29,64 +29,84 @@ end
 
 -- Opens today's daily note.
 M.open_today_daily_note = function()
-	network.post("/daily-notes/today", {})
+	exec_cmd(function()
+		network.post("/daily-notes/today", {})
+	end)
 end
 
 -- Opens the next daily note.
 M.open_next_daily_note = function()
-	network.post("/daily-notes/next", {})
+	exec_cmd(function()
+		network.post("/daily-notes/next", {})
+	end)
 end
 
 -- Opens the previous daily note.
 M.open_prev_daily_note = function()
-	network.post("/daily-notes/prev", {})
+	exec_cmd(function()
+		network.post("/daily-notes/prev", {})
+	end)
 end
 
 -- ================================= CURSOR ==================================
 
 -- Syncs the cursor position with the Obsidian app preview.
 M.scroll_into_view = function(line)
-	network.post("/editor/scroll-into-view", { line = line })
+	exec_cmd(function()
+		network.post("/editor/scroll-into-view", { line = line })
+	end)
 end
 
 -- Opens link under the cursor in the Obsidian app, and if the file exists,
 -- opens it in the current Neovim buffer.
 M.open_link = function()
-	local cursor = vim.api.nvim_win_get_cursor(0)
-	local line = cursor[1]
-	local col = cursor[2]
+	exec_cmd(function()
+		local cursor = vim.api.nvim_win_get_cursor(0)
+		local line = cursor[1]
+		local col = cursor[2]
 
-	local res = network.post("/editor/open-link", { line = line, ch = col })
-	if res ~= nil then
-		vim.cmd("edit " .. vim.fn.fnameescape(res.filepath))
-	end
+		local res = network.post("/editor/open-link", { line = line, ch = col })
+		if res ~= nil then
+			vim.cmd("edit " .. vim.fn.fnameescape(res.filepath))
+		end
+	end)
 end
 
 -- ================================== TABS ===================================
 
 -- Opens the next tab in the workspace.
 M.new_tab = function()
-	network.post("/workspace/tabs/new", {})
+	exec_cmd(function()
+		network.post("/workspace/tabs/new", {})
+	end)
 end
 
 -- Moves to the next tab (right) in the workspace if there is one.
 M.next_tab = function()
-	network.post("/workspace/tabs/next", {})
+	exec_cmd(function()
+		network.post("/workspace/tabs/next", {})
+	end)
 end
 
 -- Moves to the previous tab (left) in the workspace if there is one.
 M.prev_tab = function()
-	network.post("/workspace/tabs/prev", {})
+	exec_cmd(function()
+		network.post("/workspace/tabs/prev", {})
+	end)
 end
 
 -- Closes the current tab in the workspace.
 M.close_tab = function()
-	network.post("/workspace/tabs/close", {})
+	exec_cmd(function()
+		network.post("/workspace/tabs/close", {})
+	end)
 end
 
 -- Closes all tabs in the workspace except the current one.
 M.close_other_tabs = function()
-	network.post("/workspace/tabs/close-others", {})
+	exec_cmd(function()
+		network.post("/workspace/tabs/close-others", {})
+	end)
 end
 
 return M
