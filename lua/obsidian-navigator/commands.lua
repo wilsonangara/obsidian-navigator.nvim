2d58a2901c499beddadad0e3c2a430ff78740166
Wilson Angara <wilsonangara@gmail.com>
2025-06-08 20:48:38 +0700

    chore: add shorter abbreviated commands alternative

--- lua/obsidian-navigator/commands.lua
+++ lua/obsidian-navigator/commands.lua
@@ -36,23 +36,40 @@ M.register = function()
 	-- Plugin commands
 	vim.cmd([[
         command! ObsidianNavigatorOn lua On()
+        command! ObsNavOn lua On()
+
         command! ObsidianNavigatorOff lua Off()
+        command! ObsNavOff lua Off()
     ]])
 
 	-- Daily Notes commands
 	vim.cmd([[
-        command! ObsidianNavigatorToday lua require("obsidian-navigator.handlers").open_today_daily_note()
-        command! ObsidianNavigatorDailyNoteNext lua require("obsidian-navigator.handlers").open_next_daily_note()
-        command! ObsidianNavigatorDailyNotePrev lua require("obsidian-navigator.handlers").open_prev_daily_note()
+        command! ObsidianNavigatorTodayDailyNote lua require("obsidian-navigator.handlers").open_today_daily_note()
+        command! ObsNavDailyNoteToday lua require("obsidian-navigator.handlers").open_today_daily_note()
+
+        command! ObsidianNavigatorNextDailyNote lua require("obsidian-navigator.handlers").open_next_daily_note()
+        command! ObsNavNextDailyNote lua require("obsidian-navigator.handlers").open_next_daily_note()
+
+        command! ObsidianNavigatorPrevDailyNote lua require("obsidian-navigator.handlers").open_prev_daily_note()
+        command! ObsNavPrevDailyNote lua require("obsidian-navigator.handlers").open_prev_daily_note()
     ]])
 
 	-- Workspace tab commands
 	vim.cmd([[
         command! ObsidianNavigatorNewTab lua require("obsidian-navigator.handlers").new_tab()
+        command! ObsNavNewTab lua require("obsidian-navigator.handlers").new_tab()
+
         command! ObsidianNavigatorNextTab lua require("obsidian-navigator.handlers").next_tab()
+        command! ObsNavNextTab lua require("obsidian-navigator.handlers").next_tab()
+
         command! ObsidianNavigatorPrevTab lua require("obsidian-navigator.handlers").prev_tab()
+        command! ObsNavPrevTab lua require("obsidian-navigator.handlers").prev_tab()
+
         command! ObsidianNavigatorCloseTab lua require("obsidian-navigator.handlers").close_tab()
+        command! ObsNavCloseTab lua require("obsidian-navigator.handlers").close_tab()
+
         command! ObsidianNavigatorCloseOtherTabs lua require("obsidian-navigator.handlers").close_other_tabs()
+        command! ObsNavCloseOtherTabs lua require("obsidian-navigator.handlers").close_other_tabs()
     ]])
 end
 
